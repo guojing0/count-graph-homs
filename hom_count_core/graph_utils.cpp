e8be0504b2ba7c226366e4b2687dc191eb0791a6
Jing Guo <dev.guoj@gmail.com>
2024-01-18 16:35:20 +0100

    Update graph_utils

--- hom_count_core/graph_utils.cpp
+++ hom_count_core/graph_utils.cpp
@@ -3,6 +3,20 @@
 #include <cmath>
 
 /*
+ * Get the index of `node`
+ */
+inline int get_node_index(const Node& node) {
+    return node.first;
+}
+
+/*
+ * Get the content/vertices of `node`
+ */
+inline const std::vector<int> &get_node_content(const Node& node) {
+    return node.second;
+}
+
+/*
  * Extract the bag vertex of `index` from `mapping`
  */
 int extract_bag_vertex(int mapping, int index, int graph_size) {
@@ -28,4 +42,14 @@ int remove_vertex_from_mapping(int mapping, int index, int graph_size) {
     int right_digits = mapping % static_cast<int>(std::pow(graph_size, index));
 
     return left_digits / graph_size + right_digits;
+}
+
+bool is_valid_mapping(int mapped_intro_vtx, const std::vector<int> &mapped_nbhrs, const Graph &target_graph) {
+    for (int vtx: mapped_nbhrs) {
+        if (!boost::edge(mapped_intro_vtx, vtx, target_graph).second) {
+            return false;
+        }
+    }
+
+    return true;
 }
--- hom_count_core/graph_utils.h
+++ hom_count_core/graph_utils.h
@@ -1,8 +1,19 @@
 #ifndef HOM_COUNT_CORE_GRAPH_UTILS_H
 #define HOM_COUNT_CORE_GRAPH_UTILS_H
 
+#include <boost/graph/adjacency_list.hpp>
+#include <vector>
+
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS> Graph;
+typedef std::pair<int, std::vector<int>> Node;
+
+inline int get_node_index(const Node& node);
+inline const std::vector<int> &get_node_content(const Node& node);
+
 int extract_bag_vertex(int mapping, int index, int graph_size);
 int add_vertex_into_mapping(int new_vertex, int mapping, int index, int graph_size);
 int remove_vertex_from_mapping(int mapping, int index, int graph_size);
 
-#endif //HOM_COUNT_CORE_GRAPH_UTILS_H
+bool is_valid_mapping(int mapped_intro_vtx, const std::vector<int>& mapped_nbhrs, const Graph& target_graph);
+
+#endif
